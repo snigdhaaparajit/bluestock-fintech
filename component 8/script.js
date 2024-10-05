b3f4a7ee5667e53a9857fe66264413088bd353b9
Sasank <66859906+sasank362@users.noreply.github.com>
2024-10-05 12:32:36 +0530

    Delete component 8 directory

--- component 8/script.js
+++
@@ -1,5 +0,0 @@
-function joinCommunity() {
-    window.location.href = "https://your-redirect-url.com"; // Replace with the actual URL
-    // Or trigger an alert instead
-    // alert("Thank you for joining our community!");
-}
